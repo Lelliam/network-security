5691356844f79ec93408f90cce8d2fb46f4ffda1
whale-52hz <liuliangswust@qq.com>
2019-12-04 13:50:11 +0800

    +fade

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <AppMap></AppMap>
+    <AppTimeLine></AppTimeLine>
+    <AppRadar></AppRadar>
+    <AppCalendar></AppCalendar>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import AppMap from "./components/AppMap";
+    import AppTimeLine from "./components/AppTimeLine";
+    import AppRadar from "./components/AppRadar";
+    import AppCalendar from "./components/AppCalendar";
+    export default {
+        name: 'App',
+        components:{
+            AppMap,
+            AppTimeLine,
+            AppRadar,
+            AppCalendar
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+  }
 </style>
